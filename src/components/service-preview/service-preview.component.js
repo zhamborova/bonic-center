3df47e59a2a1c31aa2a3d282582f13897c2d6440
yasmi <yasmi.zhamborova@mail.ru>
2020-02-18 18:49:12 +0300

    home responsive css

--- src/components/service-preview/service-preview.component.js
+++ src/components/service-preview/service-preview.component.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import './service-preview.styles.css';
+import './service-preview.styles.scss';
 import Service from "../service/service.component";
 import services from "./service-list";
 
 const ServicePreview = () => {
   return (
        <div className='service-preview-wrapper'>
-           <h1>Treatments</h1>
+           <h1>Процедуры</h1>
          <div className='service-preview'>
           {services.map(service=>(<Service {...service}/>))}
       </div>
