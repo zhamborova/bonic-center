588291aa09ffd93c8e0c6bc2a1ac47eaebcba352
yasmi <yasmi.zhamborova@mail.ru>
2020-04-15 19:06:51 +0300

    service page

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import {BrowserRouter} from 'react-router-dom';
 
 ReactDOM.render(
